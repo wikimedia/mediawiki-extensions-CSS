4d8ad025b36f6eb4925ecc47c5b0f77e7b9a2e30
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-10-27 23:22:07 +0000

    Localisation updates for extension messages from Betawiki (2008-10-28 00:10 CET)

--- CSS.i18n.php
+++ CSS.i18n.php
@@ -14,3 +14,53 @@ $messages = array();
 $messages['en'] = array(
 	'css-desc' => 'A parser function that allows CSS stylesheets to be included in specific pages',
 );
+
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$messages['ar'] = array(
+	'css-desc' => 'دالة محلل تسمح بأنماط CSS أن تضمن في صفحات معينة',
+);
+
+/** Egyptian Spoken Arabic (مصرى)
+ * @author Meno25
+ */
+$messages['arz'] = array(
+	'css-desc' => 'دالة محلل تسمح بأنماط CSS أن تضمن فى صفحات معينة',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'css-desc' => 'Unha función analítica que permite que sexan incluídas en páxinas específicas follas de estilo CSS',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'css-desc' => 'Parserowa funkcija, kotraž dowola CSS stilowe předłohi do specifiskich stronow zapřijeć',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'css-desc' => 'Eng Parser-Funktioun déi et erlaabt CSS-Stilblieder a spzifesch Säiten anzebannen',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'css-desc' => "Een parserfunctie die het mogelijk maakt om CSS-stylesheets op te nemen in aangegeven pagina's",
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Minh Nguyen
+ */
+$messages['vi'] = array(
+	'css-desc' => 'Hàm trong bộ phân tích để nhúng tập tin định kiểu CSS trong một trang nói riêng',
+);
+
