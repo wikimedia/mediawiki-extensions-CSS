0a944fa1dbf2ae096206af5f0aef03b40e7a7a47
John Du Hart <johnduhart@users.mediawiki.org>
2011-10-28 15:32:00 +0000

    Committing patch by Rusty Burchfield
    Bug 31996 - CSS extension cleanup

--- CSS.php
+++ CSS.php
@@ -1,70 +1,63 @@
 <?php
 /**
- * CSS extension - A parser-function for adding CSS files, article or inline rules to articles
+ * CSS extension - A parser-function for adding CSS to articles via file,
+ * article or inline rules.
  *
- * See http://www.mediawiki.org/wiki/Extension:CSS for installation and usage details
+ * See http://www.mediawiki.org/wiki/Extension:CSS for installation and usage
+ * details.
  *
  * @file
  * @ingroup Extensions
+ * @version 2.0
  * @author Aran Dunkley [http://www.organicdesign.co.nz/nad User:Nad]
+ * @author Rusty Burchfield
  * @copyright © 2007-2010 Aran Dunkley
+ * @copyright © 2011 Rusty Burchfield
  * @licence GNU General Public Licence 2.0 or later
  */
 
 if ( !defined( 'MEDIAWIKI') ) die('Not an entry point.' );
 
-define( 'CSS_VERSION', '1.0.7, 2010-10-20' );
+define( 'CSS_VERSION', '2.0, 2011-10-27' );
 
-$wgCSSMagic = "css";
+$wgCSSMagic = 'css';
+$wgCSSPath = false;
+
 $wgHooks['ParserFirstCallInit'][] = 'wfCSSParserFirstCallInit';
 $wgHooks['LanguageGetMagic'][] = 'wfCSSLanguageGetMagic';
 
 $wgExtensionCredits['parserhook'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'CSS',
-	'author'         => '[http://www.organicdesign.co.nz/nad User:Nad]',
+	'author'         => array ( 'Aran Dunkley', 'Rusty Burchfield' ),
 	'descriptionmsg' => 'css-desc',
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:CSS',
 	'version'        => CSS_VERSION,
 );
 
-$dir = dirname( __FILE__ ) . '/';
-$wgExtensionMessagesFiles['CSS'] = $dir . 'CSS.i18n.php';
+$wgExtensionMessagesFiles['CSS'] = dirname( __FILE__ ) . '/' . 'CSS.i18n.php';
 
 function wfCSSRender( &$parser, $css ) {
-	global $wgOut, $wgRequest;
+	global $wgCSSPath, $wgScriptPath;
 
-	$parser->mOutput->mCacheTime = -1;
-	$url = false;
-	if( preg_match( '|\\{|', $css ) ) {
-
-		# Inline CSS
-		$css = htmlspecialchars( trim( Sanitizer::checkCss( $css ) ) );
-		$parser->mOutput->addHeadItem( <<<EOT
-<style type="text/css">
-/*<![CDATA[*/
-{$css}
-/*]]>*/
-</style>
-EOT
-		);
+	$css = trim( $css );
+	$title = Title::newFromText( $css );
+	if ( is_object( $title ) && $title->isKnown() ) {
+		# "blue link" Article
+		$url = $title->getLocalURL( 'action=raw&ctype=text/css' );
+		$headItem = HTML::linkedStyle( $url );
 	} elseif ( $css{0} == '/' ) {
-
-		# File
-		$url = $css;
-
+		# Regular file
+		$base = $wgCSSPath === false ? $wgScriptPath : $wgCSSPath;
+		$headItem = HTML::linkedStyle( $base . $css );
 	} else {
-
-		# Article?
-		$title = Title::newFromText( $css );
-		if( is_object( $title ) ) {
-			$url = $title->getLocalURL( 'action=raw&ctype=text/css' );
-			$url = str_replace( "&", "&amp;", $url );
-		}
+		# Inline CSS
+		$headItem = HTML::inlineStyle( Sanitizer::checkCss( $css ) );
 	}
 
-	if( $url ) $wgOut->addScript( "<link rel=\"stylesheet\" type=\"text/css\" href=\"$url\" />" );
-	return '';
+	$headItem = FormatJson::encode( $headItem );
+	$script = HTML::inlineScript( "$('head').append( $headItem );" );
+	return array( $script, 'isHTML' => true );
 }
 
 function wfCSSParserFirstCallInit( $parser ) {
