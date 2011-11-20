0e834e16437a2753097682eed92ee69074ef904d
gicode <gicode@localhost>
2011-11-20 18:48:34 +0000

    Fixes for bug 32154.  Improves security of CSS extension quite a bit.

--- CSS.php
+++ CSS.php
@@ -16,15 +16,17 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
-if ( !defined( 'MEDIAWIKI') ) die('Not an entry point.' );
+if ( !defined( 'MEDIAWIKI' ) ) die( 'Not an entry point.' );
 
-define( 'CSS_VERSION', '2.0, 2011-10-27' );
+define( 'CSS_VERSION', '3.0, 2011-11-18' );
 
 $wgCSSMagic = 'css';
 $wgCSSPath = false;
+$wgCSSIdentifier = 'css-extension';
 
 $wgHooks['ParserFirstCallInit'][] = 'wfCSSParserFirstCallInit';
 $wgHooks['LanguageGetMagic'][] = 'wfCSSLanguageGetMagic';
+$wgHooks['RawPageViewBeforeOutput'][] = 'wfCSSRawPageViewBeforeOutput';
 
 $wgExtensionCredits['parserhook'][] = array(
 	'path'           => __FILE__,
@@ -37,27 +39,73 @@ $wgExtensionCredits['parserhook'][] = array(
 
 $wgExtensionMessagesFiles['CSS'] = dirname( __FILE__ ) . '/' . 'CSS.i18n.php';
 
+$wgResourceModules['ext.CSS'] = array(
+	'scripts' => 'verifyCSSLoad.js',
+	'localBasePath' => dirname( __FILE__ ),
+	'remoteExtPath' => 'CSS',
+);
+
 function wfCSSRender( &$parser, $css ) {
-	global $wgCSSPath, $wgScriptPath;
+	global $wgCSSPath, $wgStylePath, $wgCSSIdentifier;
 
 	$css = trim( $css );
 	$title = Title::newFromText( $css );
-	if ( is_object( $title ) && $title->isKnown() ) {
-		# "blue link" Article
-		$url = $title->getLocalURL( 'action=raw&ctype=text/css' );
-		$headItem = HTML::linkedStyle( $url );
+	$rawProtection = "$wgCSSIdentifier=1";
+	$headItem = '<!-- Begin Extension:CSS -->';
+
+	if ( is_object( $title ) && $title->exists() ) {
+		# Article actually in the db
+		$params = "action=raw&ctype=text/css&$rawProtection";
+		$url = $title->getLocalURL( $params );
+		$headItem .= HTML::linkedStyle( $url );
 	} elseif ( $css[0] == '/' ) {
 		# Regular file
-		$base = $wgCSSPath === false ? $wgScriptPath : $wgCSSPath;
-		$headItem = HTML::linkedStyle( $base . $css );
+		$base = $wgCSSPath === false ? $wgStylePath : $wgCSSPath;
+		$url = wfAppendQuery( $base . $css, $rawProtection );
+
+		# Verify the expanded URL is still using the base URL
+		if ( strpos( wfExpandUrl( $url ), wfExpandUrl( $base ) ) === 0 ) {
+			$headItem .= HTML::linkedStyle( $url );
+		} else {
+			$headItem .= '<!-- Invalid/malicious path  -->';
+		}
 	} else {
-		# Inline CSS
-		$headItem = HTML::inlineStyle( Sanitizer::checkCss( $css ) );
+		# Inline CSS; use data URI to prevent injection.  JavaScript
+		# will use a canary to verify load and will safely convert to
+		# style tag if load fails.
+
+		# Generate random CSS color that isn't black or white.
+		$color = dechex( mt_rand( 1, hexdec( 'fffffe' ) ) );
+		$color = str_pad( $color, 6, '0', STR_PAD_LEFT );
+
+		# Prepend canary CSS to sanitized user CSS
+		$canaryId = "$wgCSSIdentifier-canary-$color";
+		$canaryCSS = "#$canaryId{background:#$color !important}";
+		$css = $canaryCSS . Sanitizer::checkCss( $css );
+
+		# Encode data URI and append link tag
+		$dataPrefix = 'data:text/css;charset=UTF-8;base64,';
+		$url = $dataPrefix . base64_encode( $css );
+		$headItem .= HTML::linkedStyle( $url );
+
+		# Calculate URI prefix to match link tag
+		$hrefPrefix = $dataPrefix . base64_encode( '#' . $canaryId );
+		$hrefPrefix = substr( $url, 0, strlen( $hrefPrefix ) );
+
+		# Add JS to verify the link tag loaded and fallback if needed
+		$parser->getOutput()->addModules( 'ext.CSS' );
+		$headItem .= HTML::inlineScript( <<<INLINESCRIPT
+jQuery( function( $ ) {
+	$( 'link[href^="$hrefPrefix"]' )
+		.cssExtensionDataURIFallback( '$canaryId', '$color' );
+} );
+INLINESCRIPT
+		);
 	}
 
-	$headItem = FormatJson::encode( $headItem );
-	$script = HTML::inlineScript( "$('head').append( $headItem );" );
-	return array( $script, 'isHTML' => true );
+	$headItem .= '<!-- End Extension:CSS -->';
+	$parser->getOutput()->addHeadItem( $headItem );
+	return '';
 }
 
 function wfCSSParserFirstCallInit( $parser ) {
@@ -69,5 +117,13 @@ function wfCSSParserFirstCallInit( $parser ) {
 function wfCSSLanguageGetMagic( &$magicWords, $langCode = 0 ) {
 	global $wgCSSMagic;
 	$magicWords[$wgCSSMagic] = array( $langCode, $wgCSSMagic );
+	return true;
+}
+
+function wfCSSRawPageViewBeforeOutput( &$rawPage, &$text ) {
+	global $wgCSSIdentifier;
+	if ( $rawPage->getRequest()->getBool( $wgCSSIdentifier ) ) {
+		$text = Sanitizer::checkCss( $text );
+	}
 	return true;
 }
